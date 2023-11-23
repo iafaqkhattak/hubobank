c1f2d2a4743844c426de1399568e59971d905c72
Muhammad Afaq <iafaqkhattak@gmail.com>
2023-11-24 01:46:14 +0500

    how we work done

--- src/pages/howWeWork/HowWeWork.jsx
+++ src/pages/howWeWork/HowWeWork.jsx
@@ -22,30 +22,42 @@ const HowWeWork = () => {
             <span className="get-started-txt">Get Started</span>
           </div>
         </div>
-        <div className="col-lg-6 bg-danger d-flex flex-column align-items-center justify-content-center ">
-          <div className="Star ">
-            <h2 className="">Rewards</h2>
-            <p>
-              The best credit cards offer some tantalizing <br />
-              combinations of promotions and prizes
-            </p>
-            <img src={star} alt="start-logo" />
+        <div className="col-lg-6 d-flex flex-column align-items-center justify-content-center">
+          <div className="Star logo-right-main d-flex flex-row  w-75 my-3">
+            <div className="me-4 logo-str">
+              <img classsName="" src={star} alt="start-logo" />
+            </div>
+            <div className="description">
+              <h2 className="rewards-h2">Rewards</h2>
+              <p className="rewards-para">
+                The best credit cards offer some tantalizing <br />
+                combinations of promotions and prizes
+              </p>
+            </div>
           </div>
-          <div className="Shield ">
-            <h2 className="">100% Secured</h2>
-            <p>
-              We take proactive steps make sure your <br /> information and
-              transactions are secure.
-            </p>
-            <img src={shield} alt="start-logo" />
+          <div className="Star logo-right-main d-flex flex-row secured align-items-center w-75 px-3 py-3 my-3">
+            <div className="me-4 logo-str">
+              <img classsName="" src={shield} alt="start-logo" />
+            </div>
+            <div className="description secured">
+              <h2 className="rewards-h2">100% Secured</h2>
+              <p className="rewards-para">
+                We take proactive steps make sure your <br /> information and
+                transactions are secure.
+              </p>
+            </div>
           </div>
-          <div className="Tele ">
-            <h2 className="">Balance Transfer</h2>
-            <p>
-              A balance transfer credit card can save <br /> you a lot of money
-              in interest charges.
-            </p>
-            <img src={tele} alt="start-logo" />
+          <div className="Star logo-right-main d-flex flex-row  w-75 my-3">
+            <div className="me-4 logo-str">
+              <img classsName="" src={tele} alt="start-logo" />
+            </div>
+            <div className="description">
+              <h2 className="rewards-h2">Balance Transfer</h2>
+              <p className="rewards-para">
+                A balance transfer credit card can save <br /> you a lot of
+                money in interest charges.
+              </p>
+            </div>
           </div>
         </div>
       </div>
