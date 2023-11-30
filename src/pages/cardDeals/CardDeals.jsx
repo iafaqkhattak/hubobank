c8c4344ca0e15c9517059787107cc06d999f56cb
Muhammad Afaq <iafaqkhattak@gmail.com>
2023-12-01 03:11:25 +0500

    Pay method card created

--- src/pages/cardDeals/CardDeals.jsx
+++ src/pages/cardDeals/CardDeals.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import Scan from "../images/Scan.svg";
+import ArrowDropDown from "../images/arrow_drop_down.svg";
+import Paypal from "../images/paypal.svg";
+import Apple from "../images/apple.svg";
+import Visa from "../images/visa.svg";
+import Shopify from "../images/Shopify.svg";
 
 const CardDeals = () => {
   return (
@@ -25,7 +30,7 @@ const CardDeals = () => {
             </div>
           </div>
         </div>
-        <div className=" col-lg-6 Right-Deals d-flex align-items-center  justify-content-center bg-warning">
+        <div className=" col-lg-6 Right-Deals d-flex align-items-start  justify-content-center bg-warning">
           <div className="scan-credits p-5 d-flex flex-column align-items-center">
             <div className="scan-logo d-flex justify-content-center">
               <img className=" w-75 " src={Scan} alt="scanner logo" />
@@ -39,13 +44,49 @@ const CardDeals = () => {
               </p>
             </div>
           </div>
-          <div className="pay-method d-flex flex-column border align-items-center justify-content-center bg-danger w-50 h-50">
-            <div className="txt+poly d-flex w-100 flex-row align-items-center  justify-content-between">
-              <div className="txt">Paymethod</div>
-              <div>Polygon</div>
+          <div className="pay-method d-flex flex-column  align-items-center justify-content-center  w-50 h-25 ">
+            <div className="d-flex w-75 flex-row align-items-center  justify-content-between my-3 ">
+              <div className="payment-method-para ">Pay Method</div>
+              <div>
+                <img src={ArrowDropDown} alt="Arrow Drop Down" />
+              </div>
             </div>
 
-            <div className="logos"></div>
+            <div className="logos d-flex flex-row align-items-center">
+              <div className="mx-2 white-rectangle d-flex justify-content-center ">
+                <img className="w-50" src={Paypal} alt="Paypal logo" />
+              </div>
+              <div className="mx-2 white-rectangle d-flex justify-content-center">
+                <img className="w-50" src={Apple} alt="Apple logo" />
+              </div>
+              <div className="mx-2 white-rectangle d-flex justify-content-center">
+                <img className="w-50" src={Visa} alt="Visa logo" />
+              </div>
+              <div className="mx-2 white-rectangle d-flex justify-content-center">
+                <img className="w-75" src={Shopify} alt="Shopify logo" />
+              </div>
+            </div>
+          </div>
+          <div className="Online-analysis d-flex flex-column border w-75">
+            <div className="txt+ month d-flex flex-row">
+              <div> Online Analysis</div>
+              <div className="month+poly d-flex flex-row">
+                <div>Month</div>
+                <div>Polygon</div>
+              </div>
+            </div>
+            <div className="income+expense">
+              <div className="d-flex flex-column">
+                <div>$ 2,334,67 </div>
+                <div>Income </div>
+              </div>
+              <div>
+                <div>$ 5.31M</div>
+                <div>Expenses</div>
+              </div>
+            </div>
+            <div className="grapg">Victor </div>
+            <div className="months"></div>
           </div>
         </div>
       </div>
